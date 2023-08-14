ae9ded68b2ea9170a9fc8dacee1987015d24f8c2
James Fowler <James.Fowler@community.co>
2023-08-14 00:55:13 -0400

    Updated question list and removed dollar sign from board card values

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -5,175 +5,200 @@ export const questions: Question[] = [
     id: 1,
     category: Category.Animals,
     value: 100,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is camouflage?",
+    question:
+      "What is the term for an animal's ability to change its color or pattern to match its surroundings?",
   },
   {
     id: 2,
     category: Category.Animals,
     value: 200,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is the humpback whale?",
+    question:
+      "Which mammal holds the record for the longest migration, traveling over 10,000 miles annually?",
   },
   {
     id: 3,
     category: Category.Animals,
     value: 300,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is the llama?",
+    question:
+      "Domesticated by ancient civilizations in the Andes, this South American pack animal is known for its soft wool and has been used for transportation and carrying goods for centuries.",
   },
   {
     id: 4,
     category: Category.Animals,
     value: 400,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is the dolphin?",
+    question:
+      "Able to use tools and solve complex problems, this aquatic mammal has been observed creating sponge 'tools' to protect its snout while foraging.",
   },
   {
     id: 5,
     category: Category.Animals,
     value: 500,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is the hedgehog?",
+    question:
+      "This small mammal, known for its long periods of hibernation, has the ability to drastically lower its body temperature and slow its heart rate during winter months.",
   },
   {
     id: 6,
     category: Category.Food,
     value: 100,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is 'braising'?",
+    question:
+      "What French term refers to a method of cooking food slowly in a sealed pot, typically meat with vegetables, to create a flavorful and tender dish?",
   },
   {
     id: 7,
     category: Category.Food,
     value: 200,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is sushi?",
+    question:
+      "A popular Japanese dish, this rolled delicacy consists of vinegared rice, fish, and vegetables wrapped in seaweed.",
   },
   {
     id: 8,
     category: Category.Food,
     value: 300,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is orzo?",
+    question:
+      "Originally from Italy, this type of pasta is shaped like small rice grains and is often used in soups.",
   },
   {
     id: 9,
     category: Category.Food,
     value: 400,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is a quiche?",
+    question:
+      "A savory pastry that originated in France, this dish consists of layers of flaky pastry dough filled with cheese, meats, and vegetables.",
   },
   {
     id: 10,
     category: Category.Food,
     value: 500,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is poaching?",
+    question:
+      "What term describes the process of cooking food by immersing it in a liquid at a temperature just below boiling?",
   },
   {
     id: 11,
     category: Category.Geography,
     value: 100,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is the Eiffel Tower?",
+    question:
+      "This iconic structure, located in France, was completed in 1889 and is known for its intricate lattice ironwork and panoramic views of Paris.",
   },
   {
     id: 12,
     category: Category.Geography,
     value: 200,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is the Sahara Desert?",
+    question:
+      "This vast desert, stretching across Northern Africa, is the largest hot desert in the world and is characterized by its towering sand dunes.",
   },
   {
     id: 13,
     category: Category.Geography,
     value: 300,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is the Andes?",
+    question:
+      "This mountain range, which runs through South America, is the longest in the world and features breathtaking peaks such as Aconcagua.",
   },
   {
     id: 14,
     category: Category.Geography,
     value: 400,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is Lake Superior?",
+    question:
+      "This massive lake, located in North America, is the largest freshwater lake by surface area and is shared by the United States and Canada.",
   },
   {
     id: 15,
     category: Category.Geography,
     value: 500,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is Mount St. Helens?",
+    question:
+      "Rising 12,198 feet (3,718 meters) above sea level, this active stratovolcano is located in the Pacific Northwest region of the United States.",
   },
   {
     id: 16,
     category: Category.History,
     value: 100,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "Who is Socrates?",
+    question:
+      "This ancient Athenian philosopher is known for his method of questioning and his student, Plato, who in turn taught Aristotle.",
   },
   {
     id: 17,
     category: Category.History,
     value: 200,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "Who is Christopher Columbus?",
+    question:
+      "This Italian explorer, sailing under the Spanish flag, is credited with discovering the New World when he landed in the Caribbean in 1492.",
   },
   {
     id: 18,
     category: Category.History,
     value: 300,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "Who is Leonardo da Vinci?",
+    question:
+      "This Italian artist, known for works like `The Last Supper` and the `Mona Lisa,` is considered one of the greatest painters of all time.",
   },
   {
     id: 19,
     category: Category.History,
     value: 400,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is Mesopotamia?",
+    question:
+      "This ancient civilization, located between the Tigris and Euphrates rivers, is known for its invention of writing systems and the Code of Hammurabi.",
   },
   {
     id: 20,
     category: Category.History,
     value: 500,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "Who is Constantine the Great?",
+    question:
+      "This Roman leader, known for his role in the conversion of the Roman Empire to Christianity, was the first Roman emperor to do so.",
   },
   {
     id: 21,
     category: Category.Music,
     value: 100,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is jazz?",
+    question:
+      "Originating in the African American communities of New Orleans, this genre of music is characterized by its syncopated rhythms and improvisational style.",
   },
   {
     id: 22,
     category: Category.Music,
     value: 200,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is a tuba?",
+    question:
+      "This large, low-pitched brass instrument is often featured in marching bands and provides the foundation of the bassline in many ensembles.",
   },
   {
     id: 23,
     category: Category.Music,
     value: 300,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "Who is Ludwig van Beethoven?",
+    question:
+      "This German composer, known for his masterpieces like the `Symphony No. 9` and `Für Elise,` continued to compose even after he became deaf.",
   },
   {
     id: 24,
     category: Category.Music,
     value: 400,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "What is a chorus or choir?",
+    question:
+      "This term, often used in choral music, refers to a musical composition for voices or instruments performed by an ensemble without a soloist.",
   },
   {
     id: 25,
     category: Category.Music,
     value: 500,
-    answer: "answer",
-    question: "What is the tallest animal on earth?",
+    answer: "hat is a sharp (♯)?",
+    question:
+      "This symbol in sheet music indicates that a note should be played or sung slightly higher in pitch.",
   },
 ];
